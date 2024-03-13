2062d0837cd2a793214634ce5a42a7fe0ac57bd3
asyifaa26 <asyifa.enk2654@gmail.com>
2024-03-13 13:20:36 +0700

    menambahkan class KodeBuah

---
+++ KodeBuah.cs
@@ -0,0 +1,48 @@
+﻿namespace modul4_1302223029;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+public class KodeBuah
+{
+    private Dictionary<string, string> KodePosDictionary;
+    public enum NamaBuah
+    {
+        Apel, 
+        Aprikot, 
+        Alpukat, 
+        Pisang, 
+        Paprika, 
+        Blackberry, 
+        Ceri,
+        Kelapa, 
+        Jagung,
+        Kurma, 
+        Durian,
+        Anggur,
+        Melon,
+        Semangka
+    }
+
+    public string GetKodeBuah(NamaBuah nama)
+    {
+        String[] kodebuah =
+        {
+            "A00",
+            "B00",
+            "C00",
+            "D00",
+            "E00",
+            "F00",
+            "H00",
+            "I00",
+            "J00",
+            "K00",
+            "L00",
+            "M00",
+            "N00",
+            "O00"
+        };
+
+        return kodebuah[(int)nama];
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using modul4_1302223029;
+using static modul4_1302223029.KodeBuah;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        KodeBuah kodeBuah = new KodeBuah();
+        Console.WriteLine("Kode Buah Apel: " + kodeBuah.GetKodeBuah(NamaBuah.Apel));
+        Console.WriteLine("Kode Buah Aprikot: " + kodeBuah.GetKodeBuah(NamaBuah.Aprikot));
+        Console.WriteLine("Kode Buah Alpukat: " + kodeBuah.GetKodeBuah(NamaBuah.Alpukat));
+        Console.WriteLine("Kode Buah Pisang: " + kodeBuah.GetKodeBuah(NamaBuah.Pisang));
+        Console.WriteLine("Kode Buah Paprika: " + kodeBuah.GetKodeBuah(NamaBuah.Paprika));
+        Console.WriteLine("Kode Buah Blackberry: " + kodeBuah.GetKodeBuah(NamaBuah.Blackberry));
+        Console.WriteLine("Kode Buah Ceri: " + kodeBuah.GetKodeBuah(NamaBuah.Ceri));
+        Console.WriteLine("Kode Buah Kelapa: " + kodeBuah.GetKodeBuah(NamaBuah.Kelapa));
+        Console.WriteLine("Kode Buah Jagung: " + kodeBuah.GetKodeBuah(NamaBuah.Jagung));
+        Console.WriteLine("Kode Buah Kurma: " + kodeBuah.GetKodeBuah(NamaBuah.Kurma));
+        Console.WriteLine("Kode Buah Durian: " + kodeBuah.GetKodeBuah(NamaBuah.Durian));
+        Console.WriteLine("Kode Buah Anggur: " + kodeBuah.GetKodeBuah(NamaBuah.Anggur));
+        Console.WriteLine("Kode Buah Melon: " + kodeBuah.GetKodeBuah(NamaBuah.Melon));
+        Console.WriteLine("Kode Buah Semangka: " + kodeBuah.GetKodeBuah(NamaBuah.Semangka));
+
+
+    }
+}
